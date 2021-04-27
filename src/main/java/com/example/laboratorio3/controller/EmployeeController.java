9b19836b903275669c1dfad906b3bb0fae9ca791
Hector <hector.ventura@pucp.edu.pe>
2021-04-26 20:09:32 -0500

    cositas3.0 xd

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -1,18 +1,25 @@
 package com.example.laboratorio3.controller;
 
+import com.example.laboratorio3.entity.Employees;
+import com.example.laboratorio3.repository.EmployeesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/empleado")
 public class EmployeeController {
-
+    @Autowired
+    EmployeesRepository employeesRepository;
     //COMPLETAR
     @GetMapping(value={"/lista",""})
-    public String listaEmployee(   ){
-        //COMPLETAR
+    public String listaEmployee(Model model){
+        List<Employees> lista = employeesRepository.findAll();
     }
     @GetMapping("/nuevo")
     public String nuevoEmployeeForm( ) {
