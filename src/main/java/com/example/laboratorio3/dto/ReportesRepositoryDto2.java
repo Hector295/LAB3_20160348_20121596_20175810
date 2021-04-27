dbef981840190c698c2292a4821dbe176cea1ede
sarita atanacio <atanacio.sarita@pucp.pe>
2021-04-27 00:14:05 -0500

    sarita modificaciones

--- src/main/java/com/example/laboratorio3/dto/ReportesRepositoryDto2.java
+++ src/main/java/com/example/laboratorio3/dto/ReportesRepositoryDto2.java
@@ -1,4 +1,11 @@
 package com.example.laboratorio3.dto;
 
+import java.util.Date;
+
 public interface ReportesRepositoryDto2 {
+
+    String getCountry_name();
+    String getCity();
+    int getCantidad();
+
 }
--- src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
@@ -2,6 +2,7 @@ package com.example.laboratorio3.repository;
 
 
 import com.example.laboratorio3.dto.ReportesRepositoryDto1;
+import com.example.laboratorio3.dto.ReportesRepositoryDto2;
 import com.example.laboratorio3.entity.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -20,6 +21,12 @@ public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
             "where salary > 15000", nativeQuery = true)
     List<ReportesRepositoryDto1> obtenerListaReporte1();
 
-
+    @Query(value="select country_name, city, count(cantidad) from (select c.country_name, l.city, count(e.employee_id) as cantidad\n" +
+            "from countries c\n" +
+            "inner join locations l on (c.country_id=l.country_id)\n" +
+            "inner join departments d on (d.location_id=l.location_id)\n" +
+            "inner join employees e on (e.department_id=d.department_id)\n" +
+            "group by d.department_id) as q1 where cantidad > 3 group by city", nativeQuery = true)
+    List<ReportesRepositoryDto2> obtenerListaReporte2();
 
 }
